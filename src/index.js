98e0d5393068b8007fb5794c5f9997c3ce6eba16
Taras-Veremchuk <taras.veremm@gmail.com>
2023-04-08 10:46:49 +0200

    add files road

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './sass/index.scss';
 import './js/local-storage/';
 import './js/modal-window';
-import './js/rest-api/fetch';
+import './js/restApi/fetch';
+import './js/pagination-home';
+import './js/pagination-library';
