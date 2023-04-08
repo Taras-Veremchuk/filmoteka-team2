6fd38a6bb5d69f5569e2c5189f5e226420db0c68
Taras-Veremchuk <taras.veremm@gmail.com>
2023-04-08 20:31:43 +0200

    add js files

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './sass/index.scss';
 // import './js/local-storage/';
 // import './js/modal-window';
 import './js/restAPI/restAPI';
-// import './js/pagination-home';
+import './js/pagination-home/pagination-home';
 // import './js/pagination-library';
+import './js/refs/refs';
--- src/library.js
+++ src/library.js
@@ -2,5 +2,7 @@ import './sass/index.scss';
 // import './js/local-storage/';
 // import './js/modal-window';
 import './js/restAPI/restAPI';
+import './js/pagination-home/pagination-home';
+import './js/refs/refs';
 // import './js/pagination-home';
 // import './js/pagination-library';
