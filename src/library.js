06051c1d65054d567c6faca0ac19184f595115fc
Taras-Veremchuk <taras.veremm@gmail.com>
2023-04-08 13:35:17 +0200

    change files road

--- src/library.js
+++ src/library.js
@@ -1,6 +1,6 @@
 import './sass/index.scss';
-import './js/local-storage/';
-import './js/modal-window';
-import './js/restApi/fetch';
-import './js/pagination-home';
-import './js/pagination-library';
+// import './js/local-storage/';
+// import './js/modal-window';
+import './js/restApi/restAPI';
+// import './js/pagination-home';
+// import './js/pagination-library';
