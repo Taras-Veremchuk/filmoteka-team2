1fbe5786b2f3e7de3870631acb1c4b044a43a50d
Taras-Veremchuk <taras.veremm@gmail.com>
2023-04-08 18:41:59 +0200

    error correction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/index.scss';
 // import './js/local-storage/';
 // import './js/modal-window';
-import './js/restApi/restAPI';
+import './js/restAPI/restAPI';
 // import './js/pagination-home';
 // import './js/pagination-library';
