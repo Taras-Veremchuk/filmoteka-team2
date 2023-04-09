e3085daf1a01aab5240280039fe3c54fe9bfcd84
R0m5 <rlastovetskyi@gmail.com>
2023-04-09 19:24:12 +0300

    ++++

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './sass/index.scss';
-import './js/local-storage/';
-import './js/modal-window';
-import './js/restApi/fetch';
-import './js/pagination-home';
-import './js/pagination-library';
+import './js/restAPI/restAPI.js';
+import './js/search/search';
+import './js/render-cards';
