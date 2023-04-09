3452d79f57b805ceeca27aba8a7e062790458089
denius97 <106709563+denius97@users.noreply.github.com>
2023-04-09 20:17:12 +0300

    show genres and something else

--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -1,12 +1,16 @@
 import FilmRestAPI from './restAPI/restAPI';
+import defaultPoster from '../images/default-poster.jpg';
 const cardSetEl = document.querySelector('.card-set');
 
-const getMovies = new FilmRestAPI().fetchMovies();
-getMovies.then(renderMovies).catch(err => console.log('Error: ', err));
+new FilmRestAPI()
+  .fetchMovies()
+  .then(renderMovies)
+  .catch(err => console.log('Error: ', err));
 
-function renderMovies(movies) {
-//   console.log(movies.results);
+export function renderMovies(movies) {
+  //   console.log(movies.results);
   const IMG_BASE = 'https://image.tmdb.org/t/p/w400';
+  const genresList = JSON.parse(localStorage.getItem('MOVIE_GENRES'));
   const markup = movies.results
     .map(movie => {
       const {
@@ -15,11 +19,12 @@ function renderMovies(movies) {
         genre_ids: genreIds,
         release_date: releseDate,
       } = movie;
+      const movieGenres = genreIds.map(genre => genresList[genre]);
       return `<li class="card-set__item movie-card"><a href="" class="movie-card__link"><div class="movie-card__holder"><img src=${
-        IMG_BASE + posterPath
+        posterPath ? IMG_BASE + posterPath : defaultPoster
       } alt="${title} poster" class="movie-card__img" width="100%"></div><p class="movie-card__title">${title}            
             </p><p class="movie-card__genre">${
-              genreIds.length < 3 ? genreIds : genreIds.slice(0, 2) + ', Other'
+                movieGenres.length < 3 ? movieGenres.join(', ') : movieGenres.slice(0, 2).join(', ') + ', Other'
             } | 
                 <span class="movie-card__date">${releseDate.slice(0, 4)}</span>
             </p></a></li>`;
@@ -27,7 +32,14 @@ function renderMovies(movies) {
     .join('');
 
   cardSetEl.insertAdjacentHTML('beforeend', markup);
-  
+
+  //   const links = cardSetEl.querySelectorAll('.movie-card__link');
+  cardSetEl
+    .querySelectorAll('.movie-card__link')
+    .forEach(element => element.addEventListener('click', onClick));
 }
 
-
+function onClick(evt) {
+  evt.preventDefault();
+  console.log(evt);
+}
