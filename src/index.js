091de5565efff81e62e9eb5ec8eb54cacebb24ec
R0m5 <rlastovetskyi@gmail.com>
2023-04-09 16:02:49 +0300

    Taras

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './sass/index.scss';
-// import './js/local-storage/';
-// import './js/modal-window';
-import './js/search/search';
-import './js/render-cards';
-import './js/search/search';
-
-import './js/restAPI/restAPI';
-// import './js/pagination-home/pagination-home';
-// import './js/refs/refs';
+import './js/local-storage/';
+import './js/modal-window';
+import './js/restApi/fetch';
+import './js/pagination-home';
+import './js/pagination-library';
