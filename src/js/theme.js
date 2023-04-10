c52441081ef790fc9a9c1a0fc7101140e44cff23
Nazar-Kulyk <nazarkulyk1993@gmail.com>
2023-04-10 21:41:23 +0200

    theme localstorage

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,19 +5,56 @@ const footerContainer = document.querySelector('.footer__container');
 const titleElements = document.querySelectorAll('.movie-card__title');
 const genreElements = document.querySelectorAll('.movie-card__genre');
 
+// Функція для зберігання значення теми в localStorage
+const setTheme = isDark => {
+  localStorage.setItem('isDark', isDark);
+};
+
+// Функція для отримання значення теми з localStorage
+const getTheme = () => {
+  return localStorage.getItem('isDark') === 'true';
+};
+
+// Перевіряємо, чи збережене значення теми в localStorage
+if (getTheme()) {
+  document.body.classList.add('dark');
+  theme.classList.add('active');
+  themeSwitch.classList.add('active');
+  footer.classList.add('active');
+  footerContainer.classList.add('footer--dark');
+  titleElements.forEach(title => {
+    title.classList.add('active');
+  });
+  genreElements.forEach(genre => {
+    genre.classList.add('active');
+  });
+} else {
+  document.body.classList.remove('dark');
+  theme.classList.remove('active');
+  themeSwitch.classList.remove('active');
+  footer.classList.remove('active');
+  footerContainer.classList.remove('footer--dark');
+  titleElements.forEach(title => {
+    title.classList.remove('active');
+  });
+  genreElements.forEach(genre => {
+    genre.classList.remove('active');
+  });
+}
+
+// Додаємо слухач подій
 themeSwitch.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+  const isDark = document.body.classList.toggle('dark');
   theme.classList.toggle('active');
   themeSwitch.classList.toggle('active');
   footer.classList.toggle('active');
   footerContainer.classList.toggle('footer--dark');
-
   titleElements.forEach(title => {
     title.classList.toggle('active');
   });
-
   genreElements.forEach(genre => {
     genre.classList.toggle('active');
   });
+  // Зберігаємо значення теми в localStorage
+  setTheme(isDark);
 });
-// ---------------------------
