aff8882eabb0f809cd2aee2cd0d9b4598671e891
Nazar-Kulyk <nazarkulyk1993@gmail.com>
2023-04-10 03:03:31 +0200

    add variables

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,7 +1,20 @@
 const themeSwitch = document.querySelector('#switch');
+const theme = document.querySelector('.theme');
+const footer = document.querySelector('footer');
+const titleElements = document.querySelectorAll('.movie-card__title');
+const genreElements = document.querySelectorAll('.movie-card__genre');
 
 themeSwitch.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   theme.classList.toggle('active');
   themeSwitch.classList.toggle('active');
+  footer.classList.toggle('active');
+
+  titleElements.forEach(title => {
+    title.classList.toggle('active');
+  });
+
+  genreElements.forEach(genre => {
+    genre.classList.toggle('active');
+  });
 });
