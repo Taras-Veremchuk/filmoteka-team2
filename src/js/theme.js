c55b1a23ce3cd674bb551d2bc114baae60ea869d
Nazar-Kulyk <nazarkulyk1993@gmail.com>
2023-04-10 15:13:27 +0200

    fix dark

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,30 @@
+// const themeSwitch = document.querySelector('#switch');
+// const theme = document.querySelector('.theme');
+// const footer = document.querySelector('footer');
+// const footerContainer = document.querySelector('footer__container');
+// const titleElements = document.querySelectorAll('.movie-card__title');
+// const genreElements = document.querySelectorAll('.movie-card__genre');
+
+// themeSwitch.addEventListener('click', () => {
+//   document.body.classList.toggle('dark');
+//   theme.classList.toggle('active');
+//   themeSwitch.classList.toggle('active');
+//   footer.classList.toggle('active');
+//   footerContainer.classList.toggle('active');
+
+//   titleElements.forEach(title => {
+//     title.classList.toggle('active');
+//   });
+
+//   genreElements.forEach(genre => {
+//     genre.classList.toggle('active');
+//   });
+// });
+
 const themeSwitch = document.querySelector('#switch');
 const theme = document.querySelector('.theme');
 const footer = document.querySelector('footer');
+const footerContainer = document.querySelector('.footer__container');
 const titleElements = document.querySelectorAll('.movie-card__title');
 const genreElements = document.querySelectorAll('.movie-card__genre');
 
@@ -9,6 +33,7 @@ themeSwitch.addEventListener('click', () => {
   theme.classList.toggle('active');
   themeSwitch.classList.toggle('active');
   footer.classList.toggle('active');
+  footerContainer.classList.toggle('footer--dark');
 
   titleElements.forEach(title => {
     title.classList.toggle('active');
@@ -17,10 +42,4 @@ themeSwitch.addEventListener('click', () => {
   genreElements.forEach(genre => {
     genre.classList.toggle('active');
   });
-});
-const container = document.querySelector('.footer__container');
-const link = document.querySelector('.footer__link');
-
-link.addEventListener('click', () => {
-  container.classList.add('active');
 });
