ffe9998c4ed315f20badcf2d79e215f0b3d34981
Nataliia D <113416916+SambykaLight@users.noreply.github.com>
2023-04-11 09:27:48 +0200

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,4 @@
-import { hideSpinner, showSpinner } from "./spiner";
+// import { hideSpinner, showSpinner } from "./spiner";
 
 
 const sliders = document.querySelector('.carouselbox')
@@ -58,65 +58,3 @@ async function renderTrendy() {
     scrollPerClick = 400; 
 }
 
-async function getMovieVideos(movieId) {
-    const API_KEY = '4c6e55add2f00844e9f979bd7b0fac7c';
-        const BASE_URL = `https://api.themoviedb.org/3/`;
-
-    const url = `${BASE_URL}/movie/${movieId}/videos?api_key=${API_KEY}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const result = data.results;
-}
-
-function trailerLink(eRef) {
-    const trailerBtn = eRef;
-
-    trailerBtn.forEach(el =>
-        el.addEventListener('click', e => {
-            showSpinner();
-            renderModelTrailer(e.target.dataset.id);
-        }),
-    );
-
-    function renderModelTrailer(movieId) {
-        try {
-            const response = getMovieVideos(movieId);
-            return response
-                .then(({ data }) => {
-                    const id = data.results[0].key;
-                    const instance = sliders.create(
-                        `<iframe
-    width="980"
-    height="525"
-    src='https://www.youtube.com/embed/${id}'
-    frameborder="0"
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen>
-  </iframe>`
-                    );
-                    instance.show();
-                    modalTrailer(instance);
-                    hideSpinner();
-                })
-                .catch(() => {
-                    hideSpinner();
-                    const instance = sliders.create(
-                        `<img 
-      width="980" 
-      height="525" 
-      src="https://moviemaker.minitool.com/images/uploads/articles/2020/08/youtube-video-not-available/youtube-video-not-available-1.png" 
-      alt="no found trailer" 
-      class="trailer_video">`
-                    );
-                    instance.show();
-                    modalTrailer(instance);
-                });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    function modalTrailer(instance) {
-        const modalBox = document.querySelector
-    }
-}
