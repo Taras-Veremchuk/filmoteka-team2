eb8aee287785e64f182beaf9f976e77ed894b9f7
Taras-Veremchuk <taras.veremm@gmail.com>
2023-04-11 17:55:14 +0200

    add open-modal files

--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -1,5 +1,4 @@
 import defaultPoster from '../images/default-poster.jpg';
-import { refs } from './refs/refs';
 
 export function renderMovies(movies) {
   const IMG_BASE = 'https://image.tmdb.org/t/p/w400';
@@ -16,9 +15,7 @@ export function renderMovies(movies) {
       const movieGenres = genreIds.map(genre => genresList[genre]);
       return `<li class="card-set__item movie-card" data-id=${id}><a href=""  class="movie-card__link"><div class="movie-card__holder"><img src=${
         posterPath ? IMG_BASE + posterPath : defaultPoster
-        // ДОДАВ ДВІ ТИМЧАСОВІ КНОПКИ
       } alt="${title} poster" class="movie-card__img" data-id=${id} width="100%"></div>
-      <button type="submit" class="add-watch">Add to watched</button><button type="submit" class="add-que">Add to queue</button>
         <p class="movie-card__title">${title}
             </p><p class="movie-card__genre">${
               movieGenres.length < 3
@@ -31,60 +28,4 @@ export function renderMovies(movies) {
     .join('');
 
   return markup;
-  // ЗМІНИВ ЩОБ МОЖНА БУЛО ПЕРЕВИКОРИСТОВУВАТИ
-
-  // refs.cardSetEl.innerHTML = markup;
-
-  //   const links = cardSetEl.querySelectorAll('.movie-card__link');
 }
-// =========================================================================================
-refs.cardSetEl.addEventListener('click', onClick);
-const libraryWatchedList = document.querySelector('.card-watched-library');
-const libraryQueList = document.querySelector('.card-que-library');
-
-function onClick(evt) {
-  evt.preventDefault();
-  // ПЕРЕВІРКА НА ІМГ АЛЕ ХОТІЛОСЯ Б НА ЛІШЦІ
-  const isCorrectClick = evt.target.nodeName === 'IMG';
-  if (!isCorrectClick) {
-    return;
-  }
-
-  const filmdId = evt.target.dataset.id;
-  console.log(filmdId);
-  try {
-    const allFilmsObj = JSON.parse(localStorage.getItem('CURRENT_ITEMS'));
-    const currentFilmObj = allFilmsObj.results.find(card => card.id == filmdId);
-    console.log(currentFilmObj);
-    // tryThis(currentFilmObj);
-    const btnWatchAdd = document.querySelector('.add-watch');
-    const btnQuechAdd = document.querySelector('.add-que');
-
-    btnWatchAdd.addEventListener(
-      'click',
-      onAddToWatchClick(currentFilmObj, libraryWatchedList)
-    );
-    btnQuechAdd.addEventListener(
-      'click',
-      onAddToWatchClick(currentFilmObj, libraryQueList)
-    );
-  } catch (err) {
-    console.error;
-  }
-}
-
-// function tryThis(allFilmsObj) {
-// const libraryWatchedList = document.querySelector('.card-watched-library');
-// const libraryQueList = document.querySelector('.card-que-library');
-// const btnWatchAdd = document.querySelector('.add-watch');
-// const btnQuechAdd = document.querySelector('.add-que');
-
-// btnWatchAdd.addEventListener('click', onAddToWatchClick);
-// btnQuechAdd.addEventListener('click', onAddToQueuClick);
-
-function onAddToWatchClick(allFilmsObj, place) {
-  const markupsFilms = renderMovies(allFilmsObj);
-  place.innerHTML = markupsFilms;
-}
-
-// }
