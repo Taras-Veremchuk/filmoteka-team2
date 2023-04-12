368e1ccc16d8f5c9e8fda686a6b8ac14f0476473
Taras-Veremchuk <taras.veremm@gmail.com>
2023-04-13 00:34:26 +0200

    +++

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import './sass/index.scss';
-import './js/storage/localstorage';
-// import './js/modal-window';
 import './js/restAPI/restAPI';
 import './js/pagination-home/pagination-home';
-// import './js/pagination-library';
 import './js/render-cards';
 import './js/search/search';
 import './js/theme';
 import './js/fetch-popular';
 import './js/slider';
-// import './js/modal-window/open-modal';
