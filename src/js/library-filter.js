42d6c43c14b1b9b96087a9d57a969cf2a6b6a914
denius97 <106709563+denius97@users.noreply.github.com>
2023-04-14 01:01:06 +0300

    btns in mobile

--- src/js/library-filter.js
+++ src/js/library-filter.js
@@ -2,19 +2,20 @@ import refreshLibrary from './refresh-library';
 // const watchedBtn = document.querySelector('.btn-top-watched');
 // const queueBtn = document.querySelector('.btn-top-queue');
 const libraryBtns = document.querySelector('.tab__desk-btn');
+const mobileLibraryBtns = document.querySelector('.btn-library');
 libraryBtns.addEventListener('click', filterGallery);
+mobileLibraryBtns.addEventListener('click', filterGallery);
 
 function filterGallery(evt) {
     const currentActiveBtn = evt.currentTarget.querySelector('.btn-top--active');
-
-    if(evt.target.classList.contains('btn-top-queue')) {
+const classList = evt.target.classList;
+    if(classList.contains('btn-top-queue') || classList.contains('btn__queue-bot')) {
         refreshLibrary.showQueue();
         currentActiveBtn?.classList.remove('btn-top--active');
         evt.target.classList.add('btn-top--active');
         return;
     }
-
-    if(evt.target.classList.contains('btn-top-watched')) {
+    if(classList.contains('btn-top-watched') || classList.contains('btn__watched-bot')) {    
         refreshLibrary.showWatched();
         currentActiveBtn?.classList.remove('btn-top--active');
         evt.target.classList.add('btn-top--active');
