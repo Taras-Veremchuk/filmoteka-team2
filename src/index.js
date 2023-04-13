c4479fa8380175918fcd502f0210cc74db2a01e8
denius97 <106709563+denius97@users.noreply.github.com>
2023-04-13 04:10:28 +0300

    Revert "Merge branch 'main' into render-modal"
    This reverts commit e93b372cba2bc853934197ddad54e6260eeebec6, reversing
    changes made to 15a9e6859f0d1e61ef71d78534bf5077c2fcb827.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './sass/index.scss';
+import './js/storage/localstorage';
+// import './js/modal-window';
 import './js/restAPI/restAPI';
 import './js/pagination-home/pagination-home';
+// import './js/pagination-library';
 import './js/render-cards';
 import './js/search/search';
 import './js/theme';
