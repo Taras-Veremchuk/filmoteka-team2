498415366978a89f58589ccca0a8696423d3e7c9
denius97 <106709563+denius97@users.noreply.github.com>
2023-04-14 03:48:00 +0300

    shearch library

--- src/js/library-filter.js
+++ src/js/library-filter.js
@@ -3,10 +3,33 @@ import refreshLibrary from './refresh-library';
 // const queueBtn = document.querySelector('.btn-top-queue');
 const libraryBtns = document.querySelector('.tab__desk-btn');
 const mobileLibraryBtns = document.querySelector('.btn-library');
+// const libraryInput = document.querySelector('.search-input');
+const librarySearchForm = document.querySelector('.search-form');
 libraryBtns.addEventListener('click', filterGallery);
 mobileLibraryBtns.addEventListener('click', filterGallery);
+librarySearchForm.addEventListener('submit', inputSearchGallery);
+librarySearchForm.addEventListener('input', inputSearchGallery);
+
+function inputSearchGallery(evt) {
+    evt.preventDefault(); 
+    refreshLibrary.showAll(evt.currentTarget.name.value);
+   if(evt.type === "submit") {
+        evt.target.reset();
+        refreshLibrary.filmName = '';
+            }
+}
+
+// function inputSearchGallery(evt) {
+//     evt.preventDefault(); 
+//     refreshLibrary.showAll(evt.target.value);
+// console.log(evt.target.value)
+// }
+
+
 
 function filterGallery(evt) {
+    librarySearchForm.reset();
+    refreshLibrary.filmName = '';
     const currentActiveBtn = evt.currentTarget.querySelector('.btn-top--active');
 const classList = evt.target.classList;
     if(classList.contains('btn-top-queue') || classList.contains('btn__queue-bot')) {
